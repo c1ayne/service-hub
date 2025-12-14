b32f9ebc44a3f8d6ec3e10ce56b303abb23692e6
clayne <savva665544332211@yandex.ru>
2025-12-15 01:52:14 +0300

    initial commit

---
+++ src/test/java/ru/falcons/ServiceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.falcons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
