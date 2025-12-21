8b82a3e5156ecf7519c2d1e0e6dca4e208f10552
clayne <savva665544332211@yandex.ru>
2025-12-21 05:38:44 +0300

    create service etity and entity migration

---
+++ src/main/java/ru/falcons/service/entity/ServiceEntity.java
@@ -0,0 +1,41 @@
+package ru.falcons.service.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "services")
+@Setter @Getter @NoArgsConstructor
+public class ServiceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "owner_user_id", nullable = false)
+    private Long ownerUserId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/ru/falcons/service/repository/ServiceRepository.java
@@ -0,0 +1,4 @@
+package ru.falcons.service.repository;
+
+public interface ServiceRepository {
+}
