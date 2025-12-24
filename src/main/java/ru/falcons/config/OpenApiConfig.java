ca35257960c0e783720df994af3228bf96d4af57
clayne <savva665544332211@yandex.ru>
2025-12-24 08:33:41 +0300

    create OpenApiConfig

---
+++ src/main/java/ru/falcons/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package ru.falcons.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ServiceHub API",
+                version = "1.0.0",
+                description = "API for requests and services (Java)",
+                contact = @Contact(
+                        name = "Falcons",
+                        email = "support@servicehub.com"
+                )
+        ),
+        security = @SecurityRequirement(name = "Bearer Authentication")
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "jwt",
+        description = "Enter the JWT token you received from Laravel"
+)
+public class OpenApiConfig {
+}
